76591c7589effcd7cf1117453033ed53d1f15b4d
Pritam Bag <pritam02b@gmail.com>
2025-12-26 21:20:47 +0530

    rename: change shoz-table to lab-table

--- resources/views/navigation/index.blade.php
+++ resources/views/navigation/index.blade.php
@@ -3,6 +3,6 @@
 @section('content')
 <div>
     <h5 class="mb-2 fs-6 text-muted">Navigation</h5>
-    <table id="navigations-table" class="shoz-table"></table>
+    <table id="navigations-table" class="lab-table"></table>
 </div>
 @endsection
